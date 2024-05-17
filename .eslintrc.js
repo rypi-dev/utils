3bfa061c31af541320ff2ab95dcec31d7a4a42b8
Walidoux <ma.walidkorchi@icloud.com>
2024-05-17 21:43:39 +0100

    ci(release): missing build task in job + clean up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 const { configure, presets } = require('@habbaz-dev/eslint-config')
 
 module.exports = configure({
-	presets: [presets.imports(), presets.typescript(), presets.prettier(), presets.node()]
+  presets: [presets.imports(), presets.typescript(), presets.prettier(), presets.node()]
 })
